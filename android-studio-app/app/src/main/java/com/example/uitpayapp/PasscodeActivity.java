4f18b26b27c921b172a5dc4487dfca6cba1088e7
Kiến Huy <huykiento@gmail.com>
2026-04-03 11:17:44 +0700

    #2 - Fig bug crash activity_bottom_sheet_forgot

--- android-studio-app/app/src/main/java/com/example/uitpayapp/PasscodeActivity.java
+++ android-studio-app/app/src/main/java/com/example/uitpayapp/PasscodeActivity.java
@@ -24,6 +24,7 @@ public class PasscodeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_passcode);
 
         initViews();
