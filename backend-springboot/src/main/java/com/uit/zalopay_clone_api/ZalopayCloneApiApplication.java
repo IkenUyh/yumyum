ffdef3f1e36195c8c27eff65499586bdb6cab5c1
Kiến Huy <huykiento@gmail.com>
2026-04-11 09:00:29 +0700

    #11 - Generate RESTful API Spring Boot

---
+++ backend-springboot/src/main/java/com/uit/zalopay_clone_api/ZalopayCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.uit.zalopay_clone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZalopayCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZalopayCloneApiApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/src/test/java/com/uit/zalopay_clone_api/ZalopayCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uit.zalopay_clone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZalopayCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
