06b3846a079f38da9ab28ea40c11789f81841a9d
Kiến Huy <huykiento@gmail.com>
2026-06-20 20:54:28 +0700

    fix: revert V2 migration and add V36 to safely update dummy emails

---
+++ backend-springboot/src/main/resources/db/migration/V36__Update_dummy_users_emails.sql
@@ -0,0 +1,5 @@
+-- Update dummy data with emails
+UPDATE users SET email = 'huykiento@gmail.com' WHERE phone_number = '0987301126';
+UPDATE users SET email = 'charafrisksans024@gmail.com' WHERE phone_number = '0376171242';
+UPDATE users SET email = 'pbqhuy@gmail.com' WHERE phone_number = '0329815572';
+UPDATE users SET email = 'thanhdan020809@gmail.com' WHERE phone_number = '6767676767';
