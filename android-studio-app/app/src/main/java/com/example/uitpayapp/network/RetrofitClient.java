785f597f1f5d2ff17c715fb43e22b4747d0623c4
Kiến Huy <huykiento@gmail.com>
2026-06-20 21:55:09 +0700

    fix(network): update base url to secure production domain

--- android-studio-app/app/src/main/java/com/example/uitpayapp/network/RetrofitClient.java
+++ android-studio-app/app/src/main/java/com/example/uitpayapp/network/RetrofitClient.java
@@ -35,7 +35,7 @@ import com.example.uitpayapp.BuildConfig;
 public class RetrofitClient {
     // Tự động lấy URL dựa trên Build Variant (Debug: local, Release: server)
     // Cấu hình ở trong file app/build.gradle
-    private static final String BASE_URL = "http://10.0.2.2:8083/";
+    private static final String BASE_URL = "https://kienhuy-dev.name.vn/";
     private static Retrofit retrofit = null;
     private static Context appContext = null; // Lưu trữ context toàn cục kích thước nhỏ gọn
 
